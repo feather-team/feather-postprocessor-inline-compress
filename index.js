40c20ef0a5e7d3178f7b413f43227091d3ff26c2
jsyczhanghao <jsyczhanghao@sina.com>
2014-12-18 17:00:39 +0800

    inline compress fixed bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var INLINE_COMPRESS_REG = /(<(script|style)[\s\S]+?)\b(?:feather|data)-inline-compress\b([\s\S]*?>)([\s\S]*?)<\/\2>/g;
+var INLINE_COMPRESS_REG = /(<(script|style)[\s\S]+?\b(?:feather|data)-inline-compress\b[\s\S]*?>)([\s\S]*?)<\/\2>/g;
 var ug = require('uglify-js'), clean = require('clean-css');
 
 module.exports = function (content, file, conf){
@@ -9,12 +9,12 @@ module.exports = function (content, file, conf){
 
         content = content.replace(INLINE_COMPRESS_REG, function(_0, _1, _2, _3, _4){
             if(_2 == 'script'){
-                _4 = ug.minify(content, {fromString: true}).code;
+                _3 = ug.minify(_3, {fromString: true}).code;
             }else{
-                _4 = clean.process(_4, {processImport: false});
+                _3 = clean.process(_3, {processImport: false});
             }
 
-            return _1 + _3 + _4 + '</' + _2 + '>';
+            return _1 + _3 + '</' + _2 + '>';
         });
     }
 
