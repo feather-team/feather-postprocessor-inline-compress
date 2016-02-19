badb1dc8c2738b63fe33e929a15e92bc821678d8
jsyczhanghao <jsyczhanghao@sina.com>
2016-02-19 16:48:02 +0800

    压缩时，去掉属性

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var INLINE_COMPRESS_REG = /(<(script|style)[\s\S]+?\b(?:feather|data)-inline-compress\b[\s\S]*?>)([\s\S]*?)<\/\2>/g;
+var INLINE_COMPRESS_REG = /(<(script|style)([\s\S]+?\b(?:feather|data)-inline-compress\b[^>]*)>)([\s\S]*?)<\/\2>/g;
 var ug = require('uglify-js'), clean = require('clean-css');
 
-module.exports = function (content, file, conf){
+module.exports = function(content, file, conf){
     if(file.isHtmlLike){
         var tmp;
 
         content = content.replace(INLINE_COMPRESS_REG, function(_0, _1, _2, _3, _4){
             if(_2 == 'script'){
-                _3 = ug.minify(_3, {fromString: true}).code;
+                _4 = ug.minify(_4, {fromString: true}).code;
             }else{
-                _3 = clean.process(_3, {processImport: false});
+                _4 = clean.process(_4, {processImport: false});
             }
 
-            return _1 + _3 + '</' + _2 + '>';
+            return '<' + _2 + _3.replace(/\s*(?:feather|data)-inline-compress\s*/, ' ').replace(/\s*$/, '') + '>' + _4 + '</' + _2 + '>';
         });
     }
 
